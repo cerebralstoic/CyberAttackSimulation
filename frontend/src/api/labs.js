cd70b2c6ec379f9d8541975688a39ca9c5b51ca2
cerebralstoic <vineet.rawat.ddun62019@gmail.com>
2026-02-21 19:41:56 +0530

    Added api calls and flag input"

--- frontend/src/api/labs.js
+++ frontend/src/api/labs.js
@@ -1,6 +1,7 @@
 const API = "http://localhost:5000/api/labs";
 
 import { auth } from "../firebase/firebase";
+import { markLabCompleted, markLabStarted } from "../services/user.service";
 
 async function getAuthHeader() {
   const user = auth.currentUser;
@@ -13,7 +14,7 @@ async function getAuthHeader() {
   };
 }
 
-export async function startLab(type) {
+export async function startLab(type, userId) {
   const headers = await getAuthHeader();
 
   const res = await fetch(`${API}/start`, {
@@ -26,6 +27,8 @@ export async function startLab(type) {
   if (!res.ok) {
     const err = await res.json();
     throw new Error(err.error || "Failed to start lab");
+  }else{
+    markLabStarted(userId);
   }
 
   return data;
@@ -46,4 +49,24 @@ export async function stopLab(containerName, historyId) {
   }
 
   return res.json();
+}
+
+export async function completeLab(historyId, flag, type, userId){
+  const headers = await getAuthHeader();
+
+  const res = await fetch(`${API}/complete`, {
+    method: "POST",
+    headers,
+    body: JSON.stringify({historyId, flag, type}),
+  });
+
+  const data = await res.json();
+
+  if(!res.ok){
+    throw new Error(data.error || "Failed to complete lab");
+  }else{
+    await markLabCompleted(userId);
+  }
+
+  return data;
 }
